6911a631023538c386c25cd879e4136cea96068d
Vijay Sharma <vijay.sharma@kwilt.it>
2015-11-23 21:03:13 -0500

    Added tests to upload android builds (signed and unsigned)

--- cli/src/test/java/com/testfairy/uploader/cli/MainTest.java
+++ cli/src/test/java/com/testfairy/uploader/cli/MainTest.java
@@ -5,56 +5,72 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(MockitoJUnitRunner.class)
 public class MainTest {
 	private static final String API_KEY = "7ac85171ea92ad548396846dc4310e1e625fe65a";
 	private static final String IPA_PATH = MainTest.class.getClassLoader().getResource("test-ipa.ipa").getPath();
 	private static final String APK_PATH = MainTest.class.getClassLoader().getResource("test-apk.apk").getPath();
 	private static final String INVALID_FILE = MainTest.class.getClassLoader().getResource("invalid-file.txt").getPath();
+	private static final String KEYSTORE_PATH = MainTest.class.getClassLoader().getResource("debug.keystore").getPath();
+	private static final String COMMIT_MESSAGE_FILE_PATH = MainTest.class.getClassLoader().getResource("commit.txt").getPath();
+	private static final String KEYSTORE_ALIAS = "androiddebugkey";
+	private static final String KEYSTORE_PASSWORD = "android";
 
 	@Test
 	public void returns_error_when_file_does_not_exist() {
-		Assert.assertEquals(-1, run("/Users/downloads/SampleApp.apk"));
+		assertEquals(-1, run("/Users/downloads/SampleApp.apk"));
 	}
 
 	@Test
 	public void returns_when_no_file_is_given() {
-		Assert.assertEquals(-1, run(""));
+		assertEquals(-1, run(""));
 	}
 
 	@Test
 	public void returns_when_no_file_is_given_but_contains_arguments() {
-		Assert.assertEquals(-3, run("--api-key=" + API_KEY));
+		assertEquals(-3, run("--api-key=" + API_KEY));
 	}
 
 	@Test
 	public void returns_error_when_file_does_not_exist_but_contains_arguments() {
-		Assert.assertEquals(-4, run("--api-key=" + API_KEY + " /Users/downloads/SampleApp.apk"));
+		assertEquals(-4, run("--api-key=" + API_KEY + " /Users/downloads/SampleApp.apk"));
 	}
 
 	@Test
 	public void returns_error_when_file_is_invalid() {
-		Assert.assertEquals(-5, run("--api-key=" + API_KEY + " " + INVALID_FILE));
+		assertEquals(-5, run("--api-key=" + API_KEY + " " + INVALID_FILE));
 	}
 
 	@Test
 	public void returns_error_when_api_key_is_missing() {
-		Assert.assertEquals(-1, run(IPA_PATH));
+		assertEquals(-1, run(IPA_PATH));
 	}
 
 	@Test
 	public void returns_error_when_api_key_is_empty() {
-		Assert.assertEquals(-3, run("--api-key " + IPA_PATH));
+		assertEquals(-3, run("--api-key " + IPA_PATH));
 	}
 
 	@Test
 	public void returns_success_when_viewing_usage() {
-		Assert.assertEquals(0, run("--help"));
+		assertEquals(0, run("--help"));
 	}
 
 	@Test
 	public void can_upload_ios_builds() {
-		Assert.assertEquals(0, run("--api-key=" + API_KEY + " " + IPA_PATH));
+		assertEquals(0, run("--api-key=" + API_KEY + " " + IPA_PATH));
+	}
+
+	@Test
+	public void can_upload_android_builds_signed() {
+		assertEquals(0, run("--instrumentation=on --keystore=" + KEYSTORE_PATH + " --alias=" + KEYSTORE_ALIAS + " --storepass=" + KEYSTORE_PASSWORD + " --comment=\"Cowabunga\" --metrics=memory,cpu,network --api-key=" + API_KEY + " " + APK_PATH));
+	}
+
+	@Test
+	public void can_upload_android_builds_unsigned() {
+		assertEquals(0, run("--instrumentation=off --comment=@" + COMMIT_MESSAGE_FILE_PATH + " --metrics=memory,cpu,network --api-key=" + API_KEY + " " + APK_PATH));
 	}
 
 	private int run(String input) {
