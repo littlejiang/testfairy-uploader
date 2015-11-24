8b91a4d4153e981aafc06b7d75e141d20a2f494b
Vijay Sharma <vijay.sharma@kwilt.it>
2015-11-23 23:09:40 -0500

    TESTFAIRY-1832: Allow script to be run from anywhere

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,13 @@
 #! /usr/bin/env node
 
 var exec = require('child_process').execFile;
-var os = require('os');
-var arguments = process.argv.slice(2);
-var script = (os.platform() == 'win32') ? 'cli/testfairy-uploader.bat' : 'cli/testfairy-uploader'
+var path = require('path')
 
-exec(script, arguments, function(err, stdout, stderr) {
+var arguments = process.argv.slice(2);
+var script = (process.platform == 'win32') ? './cli/testfairy-uploader.bat' : './cli/testfairy-uploader'
+var scriptPath = path.join(__dirname, '../', script);
+
+exec(scriptPath, arguments, function(err, stdout, stderr) {
   console.log(stdout);
   console.log(err);
   console.log(stderr);
