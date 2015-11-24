0f9fe46779f533ad7b38caa1da09cbce22f165bb
Vijay Sharma <vijay.sharma@kwilt.it>
2015-11-23 22:44:39 -0500

    TESTFAIRY-1832: Select script based on platform

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,11 @@
 #! /usr/bin/env node
 
 var exec = require('child_process').execFile;
+var os = require('os');
 var arguments = process.argv.slice(2);
-exec('cli/testfairy-uploader', arguments, function(err, stdout, stderr) {
+var script = (os.platform() == 'win32') ? 'cli/testfairy-uploader.bat' : 'cli/testfairy-uploader'
+
+exec(script, arguments, function(err, stdout, stderr) {
   console.log(stdout);
   console.log(err);
   console.log(stderr);
