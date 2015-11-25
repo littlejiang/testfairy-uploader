c215cc8d02584128e3998c6491c57eefd03fdd1a
Vijay Sharma <vijay.sharma@kwilt.it>
2015-11-25 17:42:27 -0500

    Moved all cli and node items out of project. Pushing snapshots to maven

--- cli/src/main/java/com/testfairy/uploader/cli/StringUtils.java
+++
@@ -1,7 +0,0 @@
-package com.testfairy.uploader.cli;
-
-public class StringUtils {
-	public static boolean isEmpty(String value) {
-		return value == null || value.trim().length() == 0;
-	}
-}
---
+++ scripts/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Deploy a jar, source jar, and javadoc jar to Sonatype's snapshot repo.
+#
+# Adapted from https://coderwall.com/p/9b_lfq and
+# http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
+
+SLUG="testfairy/testfairy-uploader"
+BRANCH="master"
+
+set -e
+
+if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
+  echo "Skipping snapshot deployment: wrong repository. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo "Skipping snapshot deployment: was pull request."
+#elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
+else
+  echo "Deploying to staging..."
+  gradle uploadArchives
+  echo "Release deployed!"
+fi
