87c77c8ae602100db09825757be0a9a4ee2423ab
Vijay Sharma <vijay.sharma@kwilt.it>
2015-11-27 10:42:44 -0500

    Signing builds for staging in maven

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@
 # http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
 
 SLUG="testfairy/testfairy-uploader"
-BRANCH="master"
+MASTER_BRANCH="master"
 
 set -e
 
@@ -14,9 +14,24 @@ if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
   echo "Skipping snapshot deployment: wrong repository. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
 elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo "Skipping snapshot deployment: was pull request."
-#elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
+elif [ "$TRAVIS_BRANCH" != "$MASTER_BRANCH" ]; then
+  echo "Deploying to snapshot..."
+  ./gradlew uploadArchives -PisRelease=false --info
+  RETVAL=$?
+  if [ $RETVAL -eq 0 ]; then
+    echo 'Completed publish!'
+  else
+    echo 'Publish failed.'
+    return $RETVAL
+  fi  
 else
   echo "Deploying to staging..."
-  ./gradlew uploadArchives
-  echo "Release deployed!"
+  ./gradlew uploadArchives -PisRelease=true -Psigning.keyId="${GPG_CLIENT_ID}" -Psigning.password="${GPG_PASSWORD}" -Psigning.secretKeyRingFile="/home/travis/build/testfairy/testfairy-uploader/scripts/secring.gpg" --info
+  RETVAL=$?
+  if [ $RETVAL -eq 0 ]; then
+    echo 'Completed publish!'
+  else
+    echo 'Publish failed.'
+    return $RETVAL
+  fi
 fi
