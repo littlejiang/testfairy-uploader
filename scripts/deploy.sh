703af3a98efbafd039a64ef8176875188ebaeec6
Vijay Sharma <vijay.sharma@kwilt.it>
2015-11-30 12:14:15 -0500

    Using Github releases for deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,27 +5,27 @@
 # Adapted from https://coderwall.com/p/9b_lfq and
 # http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
 
-SLUG="testfairy/testfairy-uploader"
-MASTER_BRANCH="master"
+# SLUG="testfairy/testfairy-uploader"
+# MASTER_BRANCH="master"
 
-set -e
+# set -e
 
-if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
-  echo "Skipping snapshot deployment: wrong repository. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
-elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo "Skipping snapshot deployment: was pull request."
-elif [ "$TRAVIS_BRANCH" != "$MASTER_BRANCH" ]; then
-  echo "Deploying to snapshot..."
-  ./gradlew uploadArchives -PisRelease=false -PsonatypeUsername="${SONATYPE_USERNAME}" -PsonatypePassword="${SONATYPE_PASSWORD}"
-  RETVAL=$?
-  if [ $RETVAL -eq 0 ]; then
-    echo 'Completed publish!'
-  else
-    echo 'Publish failed.'
-    return $RETVAL
-  fi  
-else
-  echo "Deploying to staging..."
+# if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
+#   echo "Skipping snapshot deployment: wrong repository. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
+# elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+#   echo "Skipping snapshot deployment: was pull request."
+# elif [ "$TRAVIS_BRANCH" != "$MASTER_BRANCH" ]; then
+#   echo "Deploying to snapshot..."
+#   ./gradlew uploadArchives -PisRelease=false -PsonatypeUsername="${SONATYPE_USERNAME}" -PsonatypePassword="${SONATYPE_PASSWORD}"
+#   RETVAL=$?
+#   if [ $RETVAL -eq 0 ]; then
+#     echo 'Completed publish!'
+#   else
+#     echo 'Publish failed.'
+#     return $RETVAL
+#   fi  
+# else
+#   echo "Deploying to staging..."
   ./gradlew uploadArchives -PisRelease=true -PsonatypeUsername="${SONATYPE_USERNAME}" -PsonatypePassword="${SONATYPE_PASSWORD}" -Psigning.keyId="${GPG_CLIENT_ID}" -Psigning.password="${GPG_PASSWORD}" -Psigning.secretKeyRingFile="/home/travis/build/testfairy/testfairy-uploader/scripts/secring.gpg"
   RETVAL=$?
   if [ $RETVAL -eq 0 ]; then
@@ -34,4 +34,4 @@ else
     echo 'Publish failed.'
     return $RETVAL
   fi
-fi
+# fi
--- src/main/java/com/testfairy/uploader/Config.java
+++ src/main/java/com/testfairy/uploader/Config.java
@@ -1,7 +1,7 @@
 package com.testfairy.uploader;
 
 public class Config {
-	public static final String BUILD = "20150924-b2ad18cc";
+	public static final String BUILD = "development";
 	static final String SERVER_ENDPOINT = "http://app.testfairy.com";
 	static final String HTTP_USER_AGENT = "TestFairy uploader agent-" + BUILD;
 }
