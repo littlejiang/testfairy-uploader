f6bc9e0141d32e918d0cc8cf882c87984b13330e
Vladimir Tisma <tishma@gmail.com>
2017-03-29 17:16:33 +0200

    changed server endpoint to https://upload.testfairy.com

--- src/main/java/com/testfairy/uploader/Config.java
+++ src/main/java/com/testfairy/uploader/Config.java
@@ -2,6 +2,6 @@ package com.testfairy.uploader;
 
 public class Config {
 	public static final String BUILD = "development";
-	static final String SERVER_ENDPOINT = "http://app.testfairy.com";
+	static final String SERVER_ENDPOINT = "https://upload.testfairy.com";
 	static final String HTTP_USER_AGENT = "TestFairy uploader agent-" + BUILD;
 }
